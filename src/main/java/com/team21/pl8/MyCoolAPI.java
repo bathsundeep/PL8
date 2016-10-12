82f0661f9e3dfcb4a19211460b432c76c9c30848
AustinSenseman <asensema@purdue.edu>
2016-10-12 18:34:14 -0400

    super cool test

---
+++ src/main/java/com/team21/pl8/MyCoolAPI.java
@@ -0,0 +1,21 @@
+public class MyCoolAPI {
+
+    public boolean login() {
+        System.out.println("Good job!");
+        return true;
+    }
+
+    public boolean logout() {
+        System.out.println("Goodbye!");
+        return true;
+    }
+
+    public static void doesItWork() {
+        System.out.println("It works!");
+    }
+
+    public static void main(String[] args) {
+        doesItWork();
+    }
+    
+}
