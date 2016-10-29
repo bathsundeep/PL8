b654b1179c3340a23c529261130380daf5f399e0
AustinSenseman <asensema@purdue.edu>
2016-10-29 14:11:19 -0400

    Added some stuff to UserPreferences

--- src/pl8/UserPreferences.java
+++ src/pl8/UserPreferences.java
@@ -1,9 +1,28 @@
 // POJO for storing user's preferences
-
 package pl8;
 
 public class UserPreferences {
 
+    String username;
+    List<Preference> allPreferences;
+    List<Preference> softPreferences;
+    List<Preference> hardPreferences;
+
+    public UserPreferences(String username) {
+        this.username = username;
+        this.allPreferences = new ArrayList<Preference>();
+        this.softPreferences = new ArrayList<Preference>();
+        this.hardPreferences = new ArrayList<Preference>();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     
+
 
 }
