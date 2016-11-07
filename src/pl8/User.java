b888299435d01d088bf79b49381f831506de29f5
AustinSenseman <asensema@purdue.edu>
2016-11-07 18:57:00 -0500

    Created User class to make UserLoader easier

---
+++ src/pl8/User.java
@@ -0,0 +1,20 @@
+package pl8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    private String username;
+    private String password;
+    private String email;
+    private List<UserPreferences> preferences;
+
+    public User(String username, String password, String email) {
+
+    }
+
+
+
+
+}
