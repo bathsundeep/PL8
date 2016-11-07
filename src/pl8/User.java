420b37806d39598755c95f90673fccf6aa51f591
AustinSenseman <asensema@purdue.edu>
2016-11-07 18:59:58 -0500

    Created constructor

--- src/pl8/User.java
+++ src/pl8/User.java
@@ -8,9 +8,14 @@ public class User {
     private String username;
     private String password;
     private String email;
-    private List<UserPreferences> preferences;
+    private UserPreferences preferences;
 
     public User(String username, String password, String email) {
+        this.username = username;
+        /* Encrypt password using PBKDF2 */
+		this.password = Password.getHash(password);
+        this.email = email;
+        this.preferences = new UserPreferences();
 
     }
 
