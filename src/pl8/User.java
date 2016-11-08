67ff79ba94f2787a624ea1bbaf90cbc2c1652001
AustinSenseman <asensema@purdue.edu>
2016-11-07 19:03:31 -0500

    created getters/setters

--- src/pl8/User.java
+++ src/pl8/User.java
@@ -16,10 +16,28 @@ public class User {
 		this.password = Password.getHash(password);
         this.email = email;
         this.preferences = new UserPreferences();
-
     }
 
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
+    public String getPasswordHash() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = Password.getHash(password);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    } 
 
 
 }
