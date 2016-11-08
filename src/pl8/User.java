b434df556f35933edc4337f66a5eaa2fd9989801
AustinSenseman <asensema@purdue.edu>
2016-11-07 19:05:54 -0500

    Added preference related code

--- src/pl8/User.java
+++ src/pl8/User.java
@@ -8,14 +8,14 @@ public class User {
     private String username;
     private String password;
     private String email;
-    private UserPreferences preferences;
+    private List<Ingredient> preferences;
 
     public User(String username, String password, String email) {
         this.username = username;
         /* Encrypt password using PBKDF2 */
 		this.password = Password.getHash(password);
         this.email = email;
-        this.preferences = new UserPreferences();
+        this.preferences = new ArrayList<Ingredient>();
     }
 
     public String getUsername() {
@@ -37,7 +37,38 @@ public class User {
     }
     public void setEmail(String email) {
         this.email = email;
-    } 
+    }
+
+    public void addPreference(Ingredient ingredient) {
+        preferences.add(Ingredient);
+    }
+
+    public void changePreference(Ingredient ingredient) {
+        removePreference(i.ingredient);
+        addPreference(ingredient);
+    }
+
+    public void removePreference(String ingredientName) {
+        for (Ingredient i : preferences) {
+            if (i.ingredient.equals(ingredientName)) {
+                preferences.remove(i);
+                return;
+            }
+        }
+        throw new RuntimeException("Ingredient with name " + ingredientName + " does not exist");
+    }
+
+    public void removeAllPreferences() {
+        preferences.clear();
+    }
+
+    public int preferenceCount() {
+        return preferences.size();
+    }
+
+    public static boolean isSoftPreference(Ingredient i) {
+        return (i.amount > 0);
+    }
 
 
 }
